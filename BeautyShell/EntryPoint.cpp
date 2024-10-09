8841a645644d3996dae33a7a6ac7b69025cb18d2
Joao Marcos Souza Fernandes <71437696490@AD.PETROLINA.IFSERTAO-PE.EDU.BR>
2024-10-09 17:45:56 -0300

    User input setup

--- BeautyShell/EntryPoint.cpp
+++ BeautyShell/EntryPoint.cpp
@@ -1,29 +1,56 @@
 #include <raylib.h>
 #include <iostream>
+#include <vector>
 #include <string>
+
+struct Screen_Text {
+	std::string Text = "";
+	Vector2 Pos = { 0, 0 };
+	Color color = WHITE;
+};
 
 int main() {
 
 	InitWindow(500, 600, "MySole");
 	SetTargetFPS(60);
 
-	std::string Message = "Bem-vindo ao MySole!";
-	Vector2 MessagePos = {10, 10};
+	std::vector<Screen_Text> OutputText;
+
+	OutputText.push_back({ "Bem-vindo ao BeautyShell!", {10, 10}, GREEN });
+	OutputText.push_back({ "Linha de comando: ", {10, 28}, WHITE });
+
+	Screen_Text InputText;
+	InputText.Pos = { (float)(OutputText.back().Text.size() * 8 + 8), 28 };
 
 	Font arial = LoadFontEx("assets/fonts/CascadiaCode.ttf", 32, NULL, 250);
 
 	while (!WindowShouldClose()) {
 		
+		int key = GetCharPressed();
+
+		if (key >= 32 && key <= 125) {
+			InputText.Text += (char) key;
+		}
+
+		if (IsKeyPressed(KEY_BACKSPACE) && !InputText.Text.empty()) {
+			InputText.Text.pop_back();
+		}
+
 		BeginDrawing();
 
 		ClearBackground(BLACK);
 
-		DrawTextEx(arial, Message.c_str(), MessagePos, 16, 0, GREEN);
+		for (auto OutText : OutputText) {
+			DrawTextEx(arial, OutText.Text.c_str(), OutText.Pos, 16, 0, OutText.color);
+		}
+
+		DrawTextEx(arial, InputText.Text.c_str(), InputText.Pos, 16, 0, InputText.color);
 
 		EndDrawing();
 
 	}
 
+	UnloadFont(arial);
 	CloseWindow();
 
 	return 0;
